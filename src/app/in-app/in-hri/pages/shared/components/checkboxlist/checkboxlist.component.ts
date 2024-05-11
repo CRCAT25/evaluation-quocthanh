4baefa55dc0b05d56623831b65483077b1aa70d4
doquocthanh <doquocthanh@github.com>
2024-05-11 13:48:10 +0700

    evaluation - app\in-app\in-hri\pages\shared\components - add: constructor default for list status

--- src/app/in-app/in-hri/pages/shared/components/checkboxlist/checkboxlist.component.ts
+++ src/app/in-app/in-hri/pages/shared/components/checkboxlist/checkboxlist.component.ts
@@ -1,15 +1,15 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DTOStatus } from '../../dtos/DTOStatus.dto';
 
-class statusCheckObj{
+class statusCheckObj {
   statusObj: DTOStatus
   isChecked: boolean
-} 
+}
 
 /**
  * - Component provide for display list checkbox status on type of data is object DTOStatus
- * - Having 2 inputs: listCheckBox: DTOStatus[], listCheckBox: DTOStatus[]
- * - Having 1 output: listCheckBox: DTOStatus[]
+ * - Having 2 inputs: listCheckBox: DTOStatus[], listCheckBoxDefault: DTOStatus[], 
+ * - Having 1 output: listChecked: DTOStatus[]
  */
 
 @Component({
@@ -21,50 +21,68 @@ export class CheckboxlistComponent implements OnInit {
   @Input() listCheckBox: DTOStatus[] = [];
   @Input() listCheckBoxDefault: DTOStatus[] = [];
   @Output() listChecked: EventEmitter<DTOStatus[]> = new EventEmitter<DTOStatus[]>();
-  
+
   currentListCheckBoxStatus: statusCheckObj[] = [];
 
 
   ngOnInit(): void {
     this.currentListCheckBoxStatus = this.listCheckBox.map(statusObj => ({
       statusObj,
-      isChecked: false
+      isChecked: this.containsObjectStatus(this.listCheckBoxDefault, statusObj)
     }))
   }
+
+
+
+  /**
+   * This function help us check arr include statusObj or not
+   * @param arr list object status on type DTOStatus
+   * @param statusObj object status on type DTOStatus
+   * @returns true or false
+   */
+  containsObjectStatus(arr: DTOStatus[] , statusObj: DTOStatus) {
+    return arr.some(obj => obj.id === statusObj.id && obj.status === statusObj.status);
+  }
+
 
 
   /**
    * 
    * @param objStatus A object statusCheckObj
    * This function help us change value isChecked in statusCheckObj 
-   * and then call function push to emit list status on type DTOStatus to its parent component
+   * and then call function push to emit list status is checked on type DTOStatus to its parent component
    */
   onChangeCheckBox(objStatus: statusCheckObj) {
-    if(objStatus){
+    if (objStatus) {
       this.currentListCheckBoxStatus.forEach(status => {
-        if((status.statusObj).status === (objStatus.statusObj).status){
+        if ((status.statusObj).status === (objStatus.statusObj).status) {
           objStatus.isChecked = !objStatus.isChecked;
           this.pushListCheckBoxChecked();
         }
       })
     }
-    else{
+    else {
       console.error('Invalid status!');
     }
   }
-  
+
 
 
   /**
-   * This funciton help us emit list status on type DTOStatus to its parent component
+   * This funciton help us emit list status is checked on type DTOStatus to its parent component
    */
-  pushListCheckBoxChecked(){
+  pushListCheckBoxChecked() {
     let listTemp: DTOStatus[] = [];
     this.currentListCheckBoxStatus.forEach(statusObj => {
-      if(statusObj.isChecked){
+      if (statusObj.isChecked) {
         listTemp.push(statusObj.statusObj);
       }
     })
-    this.listChecked.emit(listTemp);
+    if (listTemp.length === 0) {
+      this.listChecked.emit(this.listCheckBox);
+    }
+    else {
+      this.listChecked.emit(listTemp);
+    }
   }
 }
